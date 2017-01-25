5bb700846d2bd8c14dd2252a0602fadaa777ebe6
Ryohei Shima <shima-r@sgnet.co.jp>
2017-01-25 16:03:37 +0900

    enable to load app/api.

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module RailsGrapeApiExample
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
   end
 end
