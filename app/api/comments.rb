ce0cf8e5142908aa293ea44612e6f0d5a9bf0137
Ryohei Shima <shima-r@sgnet.co.jp>
2017-01-25 18:49:48 +0900

    implement the comments api.

--- app/api/comments.rb
+++ app/api/comments.rb
@@ -1,2 +1,27 @@
 class Comments < Grape::API
+  helpers do
+    def set_article!
+      @article = Article.find_by_id(params[:article_id])
+      error!('Article Not found.', 404) unless @article
+    end
+  end
+
+  resource :articles do
+    route_param :article_id do
+      resource :comments do
+        get do
+          set_article!
+          @comments = @article.comments
+        end
+
+        params do
+          requires :comment, type: String
+        end
+        post do
+          set_article!
+          @comment = @article.comments.create(declared(params, include_missing: false))
+        end
+      end
+    end
+  end
 end
